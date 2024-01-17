16fb2f17f5d4d7d2156e94d0bb3d2c676c5d9ec0
Morteza Omar Mohammadi <32927231+mortezaom@users.noreply.github.com>
2024-01-18 03:11:58 +0430

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -112,10 +112,21 @@ return [
     ],
     'mysql' => [
         'driver' => 'mysql',
-        'host' => env('DB_HOST', '172.17.0.1'),
+        'url' => env('DATABASE_URL'),
+        'host' => env('DB_HOST', '95.216.199.131'),
         'port' => env('DB_PORT', '3306'),
         'database' => env('DB_DATABASE', 'fleetbase'),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', 'Db@PAssFleet'),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'strict' => true,
+        'engine' => null,
+        'options' => extension_loaded('pdo_mysql') ? array_filter([
+            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        ]) : [],
     ],
 ];
